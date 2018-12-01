2d1dabc6775e846a3721b3ead07ad5d2a9cac5e4
Hannes Nevalainen <kwando@merciless.me>
2018-12-01 10:54:16 +0100

    Day 1 part 2

--- 1/1.exs
+++ 1/1.exs
@@ -2,10 +2,35 @@
 
 defmodule AOC2018.Day1 do
   def part1 do
-    IO.stream(:stdio, :line)
-    |> Stream.map(&to_integer/1)
+    stream_changes()
     |> Enum.reduce(&sum/2)
     |> IO.inspect(label: "result")
+  end
+
+  def part2 do
+    freqs = stream_changes()
+    |> Enum.into([])
+
+    try do
+      Stream.cycle(freqs)
+      |> Enum.reduce({0, MapSet.new([0])}, &find_frequency/2)
+    catch
+      x -> IO.puts("result:  #{inspect(x)}")
+    end
+  end
+
+
+  def find_frequency(change, {current_freq, found_freq}) do
+    new_frequency = current_freq + change
+    if MapSet.member?(found_freq, new_frequency) do
+      throw(new_frequency)
+    end
+    {new_frequency, MapSet.put(found_freq, new_frequency)}
+  end
+
+  defp stream_changes() do
+    IO.stream(:stdio, :line)
+    |> Stream.map(&to_integer/1)
   end
 
   defp sum(value, sum), do: sum + value
@@ -14,4 +39,4 @@ defmodule AOC2018.Day1 do
   defp to_integer(value), do: value |> String.trim() |> String.to_integer()
 end
 
-AOC2018.Day1.part1()
+AOC2018.Day1.part2()
